52a723b0e2e2638150dcbf3e3730586cc7a2ea48
Roy <pawit2540@hotmail.com>
2022-11-21 19:09:16 +0700

    fix error

--- src/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.app = void 0;
-var express_1 = require("express");
-exports.app = (0, express_1["default"])();
-exports.app.get('/', function (req, res) {
-    res.send('Express + TypeScript Server (v1.0.0)');
-});
